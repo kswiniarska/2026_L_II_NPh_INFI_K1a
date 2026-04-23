206fdd6bb12fe6c2f5c3afb2f8b855f7f577c6bc
kswiniarska <karinaswiniarska@gmail.com>
2026-04-23 11:21:46 +0200

    usuwam blad

--- hello_world/views.py
+++ hello_world/views.py
@@ -3,7 +3,7 @@ from hello_world.formater import get_formatted
 from hello_world.formater import SUPPORTED, PLAIN
 from flask import request
 
-moje_imie = "Blad"
+moje_imie = "Karina"
 msg = "Hello World!"
 
 
